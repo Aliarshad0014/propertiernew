c7dde9165a380e8c2716ac0da6cd27d1dc7c408d
Awais <asrncy@gmail.com>
2024-09-02 17:27:03 +0500

    profile and banners

--- app/material/page.js
+++ app/material/page.js
@@ -1,9 +1,18 @@
 import React from "react";
-
+import comingsoon from "@/image/comingsoon.svg";
+import Image from "next/image";
 export default function page() {
   return (
     <div className="h-screen flex items-center justify-center text-black">
-      Coming Soon...
+      {/* <div className="flex items-center justify-center"> */}
+      <Image
+        src={comingsoon}
+        alt="coming soon"
+        width={100}
+        height={100}
+        className="w-full h-full object-cover"
+      />
+      {/* </div> */}
     </div>
   );
 }
