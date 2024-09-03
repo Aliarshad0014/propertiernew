b7adf4caba66a8dd37e2e8e5245cecd68f59e470
Awais <asrncy@gmail.com>
2024-09-03 16:15:33 +0500

    fixes

--- app/page.js
+++ app/page.js
@@ -106,8 +106,7 @@ export default function Home() {
               width={100}
               height={100}
               className="w-full h-full object-fill"
-              priority
-              layout="responsive"
+              loading="lazy"
             />
           </div>
         ))}
