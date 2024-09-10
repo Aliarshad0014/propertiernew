4a331d67d59a40b3f11ce79f2c02af7c16be6a84
Awais Rizwan <asrncy@gmail.com>
2024-09-10 16:22:54 +0500

    ads

--- app/layout.js
+++ app/layout.js
@@ -56,14 +56,7 @@ export default function RootLayout({ children }) {
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9403488694655871"
           crossOrigin="anonymous"></Script>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9403488694655871"
-          crossOrigin="anonymous"></Script>
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9403488694655871"
-          crossOrigin="anonymous"></Script>
+
         <div className="">
           <ChatContext.Provider
             value={{
